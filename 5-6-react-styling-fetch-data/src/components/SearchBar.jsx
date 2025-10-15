08ebc7e82a492dbab570a0cc18866e12034ce609
darkwinTech <s202257160@kfupm.edu.sa>
2025-10-15 11:35:26 +0300

    1.3

--- 5-6-react-styling-fetch-data/src/components/SearchBar.jsx
+++ 5-6-react-styling-fetch-data/src/components/SearchBar.jsx
@@ -1,9 +1,16 @@
+// TODO 1.3: Add bootstrap in Search Bar Component
+// File: src/components/SearchBar.jsx
+//
+// - Add Bootstrap classes:
+//     In div tag add properties: mb-4
+
+
 import React from 'react'
 import { Form, InputGroup } from 'react-bootstrap'
 
 function SearchBar({ searchTerm, onSearchChange }) {
   return (
-    <div className="">
+    <div className="mb-4">
       <InputGroup>
         <InputGroup.Text></InputGroup.Text>
         <Form.Control
