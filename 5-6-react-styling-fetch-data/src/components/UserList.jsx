7a9426deae071b4603768f3673a2fdaf647a0281
darkwinTech <s202257160@kfupm.edu.sa>
2025-10-15 11:37:55 +0300

    1.4

--- 5-6-react-styling-fetch-data/src/components/UserList.jsx
+++ 5-6-react-styling-fetch-data/src/components/UserList.jsx
@@ -1,3 +1,16 @@
+// TODO 1.4: Display User Cards in Grid
+// File: src/components/UserList.jsx
+// Use:
+//     - Use <Alert variant="info"> when no users are found.
+// - Make a return statement after the if condition.
+//     - Make a <Row> tag inside the return statement.
+//     - Add  user.map function inside the row tag
+// {users.map(user => (
+// ))}
+// - Add Col tag inside the user.map function.
+// - Add <UserCard user={user} onUserClick={onUserClick} /> inside the Col tag.
+// - Add properties to Col tag: md={6} lg={4} mb-4
+
 import React from 'react'
 import { Row, Col, Alert } from 'react-bootstrap'
 import UserCard from './UserCard'
@@ -5,11 +18,19 @@ import UserCard from './UserCard'
 function UserList({ users, onUserClick }) {
   if (users.length === 0) {
     return (
-      <Alert>
+      <Alert variant="info">
         No users found matching your search criteria.
       </Alert>
     )
   }
+  return (
+      <Row>  {users.map(user => (
+          <Col key={user.id} md={6} lg={4} className="mb-4">
+              <UserCard user={user} onUserClick={onUserClick} />
+          </Col>
+      ))}
+      </Row>
+  )
 
 }
 
